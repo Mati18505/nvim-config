85089992952a60b7ed29a263d4d66814c53a9690
Mateusz Stulczewski <mstulczewski@gmail.com>
2026-02-20 23:34:23 +0100

    init configuration using LazyVim

---
+++ init.lua
@@ -0,0 +1,25 @@
+vim.cmd [[
+call plug#begin()
+Plug 'ThePrimeagen/vim-be-good'
+Plug 'mbbill/undotree'
+call plug#end()
+]]
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
+require("config.lazy")
+
+vim.cmd.colorscheme("habamax")
+
+-- Skróty klawiszowe
+vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-u>', ':UndotreeToggle<CR>', { noremap = true, silent = true })
+
+-- Undo persistent
+if vim.fn.has("persistent_undo") == 1 then
+    vim.opt.undodir = vim.fn.expand("$HOME/.undodir")
+    vim.opt.undofile = true
+end
