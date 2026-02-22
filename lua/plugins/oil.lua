930b625c91782fcb955290c15031b675fc628436
Mateusz Stulczewski <mstulczewski@gmail.com>
2026-02-22 22:27:16 +0100

    fix: shortcut conflict, style

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,18 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+	'stevearc/oil.nvim',
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		keymaps = {
+			["<leader>p"] = "actions.preview",
+			["<C-p>"] = function ()
+				require('telescope.builtin').git_files()
+			end
+		}
+	},
+	-- Optional dependencies
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
 }
