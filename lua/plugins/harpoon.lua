99b5c264636f3ea53039a963d7b88bd5cda3349a
Mateusz Stulczewski <mstulczewski@gmail.com>
2026-03-01 14:02:59 +0100

    feat: remove file entries in harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,25 +13,38 @@ return {
     harpoon:setup()
 
     local function toggle_telescope(harpoon_list)
-        local file_paths = {}
+      local finder = function()
+        local paths = {}
         for _, item in ipairs(harpoon_list.items) do
-            if item and item.value and item.value ~= "" then
-                table.insert(file_paths, item.value)
-            end
+          table.insert(paths, item.value)
         end
 
-        require("telescope.pickers").new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table({
-                results = file_paths,
-            }),
-            previewer = telescope_conf.file_previewer({}),
-            sorter = telescope_conf.generic_sorter({}),
-        }):find()
+        return require("telescope.finders").new_table({
+          results = paths,
+        })
+      end
+
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = finder(),
+        previewer = telescope_conf.file_previewer({}),
+        sorter = telescope_conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+          map("i", "<C-d>", function()
+            local state = require("telescope.actions.state")
+            local selected_entry = state.get_selected_entry()
+            local current_picker = state.get_current_picker(prompt_bufnr)
+
+            table.remove(harpoon_list.items, selected_entry.index)
+            current_picker:refresh(finder())
+          end)
+          return true
+        end,
+      }):find()
     end
 
     vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, {
-        desc = "Open harpoon window",
+      desc = "Open harpoon window",
     })
 
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Mark file with harpoon" })
